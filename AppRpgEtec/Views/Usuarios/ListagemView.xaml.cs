e13e4813ec2abf5468f4b25771148a3ba2641931
Vitin silva <vitorsilvadeoliveira050@gmail.com>
2023-09-20 22:13:42 -0300

    PIN No Mapa

---
+++ AppRpgEtec/Views/Usuarios/ListagemView.xaml.cs
@@ -0,0 +1,11 @@
+using AppRpgEtec.ViewModels.Usuarios;
+
+namespace AppRpgEtec.Views.Usuarios;
+
+public partial class ListagemView : ContentPage
+{
+	public ListagemView()
+	{
+		InitializeComponent();
+	}
+}
